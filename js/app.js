40c1bf122beaf5d59850850b80edf6ba6c2fa71f
Chris Hawk <chrishawk9@gmail.com>
2017-12-14 08:54:05 -0700

    refactor: changed enemy to class syntax

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var allEnemies = [new Enemy(),new Enemy(), new Enemy()];
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+/**
+ * Enemies our player must avoid.
+ */
+class Enemy {
+    constructor() {
+        this.sprite = 'images/enemy-bug.png';
+        this.location;
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    /**
+     * Update the enemy's position. Movements multiplied by the dt parameter
+     * to ensure the game runs at the same speed on all computers.
+     * @param dt
+     */
+    update = function (dt) {
+        console.log('update');
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    };
+
+    /**
+     * Draw the enemy on the screen.
+     */
+    render = function () {
+        console.log('render');
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+}
+
 
 // Now write your own player class
 // This class requires an update(), render() and
